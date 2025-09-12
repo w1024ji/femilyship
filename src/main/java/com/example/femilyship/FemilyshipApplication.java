ebd4ac2c15d6755f3e0d9ce20ca137e7d2b9e598
Wonji Lee <w1024ji@gmail.com>
2025-09-12 11:28:43 +0900

    Initial commit

---
+++ src/main/java/com/example/femilyship/FemilyshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.femilyship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemilyshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemilyshipApplication.class, args);
+	}
+
+}
